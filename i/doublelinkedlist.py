842d7ad398a551718ca47a554cd8955c62d27e02
Pranav-4337 <rpy9114@gmail.com>
2025-02-12 15:59:24 +0530

    push

--- i/doublelinkedlist.py
+++ i/doublelinkedlist.py
@@ -19,15 +19,94 @@ class DoublyLinkedList:
             self.tail.next = newnode
             self.tail = newnode
 
+    def insertatbegin(self,data):
+        newnode = node(data)
+        if self.head is None:
+            self.head = newnode
+            self.tail = newnode
+        else:
+            newnode.next = self.head
+            self.head.prev = newnode
+            self.head = newnode
+
+    def insertatpos(self,pos,data):
+        newnode = node(data)
+        temp = self.head
+        while pos-1 > 0:
+            temp=temp.next
+            pos=pos-1
+        newnode.prev=temp
+        newnode.next=temp.next
+        temp.next.prev=newnode
+        temp.next=newnode
+
+    def deleteend(self):
+        if self.head is None:
+            print("List is empty")
+        elif self.head.next is None:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail=self.tail.prev
+            self.tail.next=None
+
+    def deletebegin(self):
+        if self.head is None:
+            print("List is empty")
+        elif self.head.next is None:
+            self.head = None
+            self.tail = None
+        else:
+            self.head=self.head.next
+            self.head.prev=None
+
+    def deletepos(self,pos):
+        if pos==1:
+            self.head=self.head.next
+            self.head.prev=None
+        else:
+            temp=self.head
+            while (pos-2 !=0):
+                temp=temp.next
+                pos=pos-1
+            temp.next=temp.next.next
+            temp.next.prev=temp
+
     def display(self):
-        current = self.head
-        while current:
-            print(current.data,end="<-->")
-            current = current.next
+        temp = self.head
+        while temp!=None:
+            print(temp.data,end="<-->")
+            temp = temp.next
+
+    def displayreverse(self):
+        temp = self.tail
+        while temp!=None:
+            print(temp.data,end="<-->")
+            temp = temp.prev
 
 dll = DoublyLinkedList()
 dll.insertatend(1)
 dll.insertatend(2)
 dll.insertatend(3)
 dll.insertatend(4)
+dll.insertatbegin(5)
+dll.insertatpos(2,6)
+dll.display()
+print("\n\n")
+dll.displayreverse()
+print("\n")
+dll.display()
+dll.deleteend()
+print("\n\n")
+dll.display()
+print("\n\n")
+dll.displayreverse()
+print("\n")
+dll.deletebegin()
+print("\n\n")
+dll.display()
+print("\n\n")
+dll.displayreverse()
+dll.deletepos(2)
+print("\n\n")
 dll.display()
