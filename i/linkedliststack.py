1e07a320ff8025e25bdb6748aae3a4e37971351c
Pranav-4337 <rpy9114@gmail.com>
2025-02-13 12:26:35 +0530

    push

--- i/linkedliststack.py
+++ i/linkedliststack.py
@@ -3,7 +3,7 @@ class node:
         self.data = data
         self.next = None
 class stack:
-    # capacity = int(input("\nEnter the capacity of stack: "))
+
     top = 0
 
     def __init__(self):
